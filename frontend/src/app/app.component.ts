95161ceb0bc8f291ab9ecc78d28d5ea35141e5cc
KaraniAbdellah <abdellahkarani@gmail.com>
2025-05-09 19:49:02 +0100

    add essentiel pages and components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'DabaStock';
 }
