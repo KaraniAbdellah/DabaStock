d00b99bc865c097bd02edd9ee8ca42253e1a6144
KaraniAbdellah <abdellahkarani@gmail.com>
2025-05-10 14:13:18 +0100

    fix header and add header services

--- frontend/src/app/component/header/header.component.ts
+++ frontend/src/app/component/header/header.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatIconModule, } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, MatIconModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  down_eles = [{title: "Log out", icon: "logout"}, {title: "Profile", icon: "person"}];
+
+  set_show_model() {
+    this.show_model.update((value: Boolean) => value = !value)
+  }
+  show_model = signal(false)
+  constructor() { }
 }
+
