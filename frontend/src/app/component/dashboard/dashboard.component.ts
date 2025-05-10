844c82b80c27b4a72478f261ede4cc5f256d16c5
KaraniAbdellah <abdellahkarani@gmail.com>
2025-05-10 23:46:39 +0100

    add dashbord

--- frontend/src/app/component/dashboard/dashboard.component.ts
+++ frontend/src/app/component/dashboard/dashboard.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterModule],
+  imports: [RouterModule, MatIconModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+  product_number = signal(10)
+  stock_number = signal(10)
+  supplier_number = signal(10)
 }
