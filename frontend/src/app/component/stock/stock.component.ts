1f6b6317b19bc86ae64bb1a2b895aa9a486de89c
KaraniAbdellah <abdellahkarani@gmail.com>
2025-05-12 21:18:45 +0100

    add featre for update and delete in stock

--- frontend/src/app/component/stock/stock.component.ts
+++ frontend/src/app/component/stock/stock.component.ts
@@ -19,11 +19,13 @@ export class StockComponent {
     color: 'red',
   });
 
+  form_state: String = 'Create Product';
   product_name: string = '';
   description: string = '';
   quantity: string = '0';
   price: string = '0';
   supplier: string = '';
+  current_product_id: Number | null = null;
 
   add_product() {
     if (
@@ -33,13 +35,38 @@ export class StockComponent {
       this.price &&
       this.supplier
     ) {
-      this.products.push({
-        product_name: this.product_name,
-        description: this.description,
-        quantity: this.quantity,
-        price: this.price,
-        supplier: this.supplier,
-      });
+      if (this.form_state == 'Create Product') {
+        this.products.push({
+          product_id: Date.now(),
+          product_name: this.product_name,
+          description: this.description,
+          quantity: this.quantity,
+          price: this.price,
+          supplier: this.supplier,
+        });
+      } else {
+        // Find the index of the product that will be updated
+        const productIndex = this.products.findIndex(
+          (product) => product.product_id === this.current_product_id
+        );
+
+        if (productIndex !== -1) {
+          // Update the product
+          this.products[productIndex] = {
+            ...this.products[productIndex],
+            product_name: this.product_name,
+            description: this.description,
+            quantity: this.quantity,
+            price: this.price,
+            supplier: this.supplier,
+          };
+        }
+
+        // Reset form to create (Default)
+        this.form_state = 'Create Product';
+        this.current_product_id = null;
+      }
+
       this.submission_status().status = false;
 
       // Reset All Fields
@@ -54,10 +81,28 @@ export class StockComponent {
     }
   }
 
-  update_product() {
-    
-  }
-  delete_product() {
+  update_product(product_id: Number) {
+    console.log(product_id);
+    console.log(this.products);
 
+    // Set The Info in Form
+    let updatedProduct: any = this.products.find(
+      (product) => product.product_id == product_id
+    );
+    this.form_state = 'Update Product';
+    this.product_name = updatedProduct.product_name;
+    this.description = updatedProduct.description;
+    this.quantity = updatedProduct.quantity;
+    this.price = updatedProduct.price;
+    this.supplier = updatedProduct.supplier;
+    this.current_product_id = product_id;
+  }
+
+  delete_product(product_id: Number) {
+    console.log(this.products);
+    this.products = this.products.filter(
+      (product) => product_id != product.product_id
+    );
+    console.log(this.products);
   }
 }
