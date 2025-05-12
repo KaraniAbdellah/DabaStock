ac34df329accd00eec2fcf92057df75512e98e33
KaraniAbdellah <abdellahkarani@gmail.com>
2025-05-12 22:06:20 +0100

    add feature of edit and delete suppliers and also authentification feature

--- frontend/src/app/pages/auth/auth.component.ts
+++ frontend/src/app/pages/auth/auth.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auth',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, MatIconModule, CommonModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.css',
 })
 export class AuthComponent {
-  
+  isLogin = true;
+
+  toggleForm() {
+    this.isLogin = !this.isLogin;
+  }
 }
