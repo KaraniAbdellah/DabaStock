32a7617ce202c881cf986f7f0877ab1eca4e2b9e
KaraniAbdellah <abdellahkarani@gmail.com>
2025-06-09 00:05:44 +0100

    start making controller

---
+++ backend/dabactock/src/main/java/dabastock/om/dabactock/controllers/SupplierController.java
@@ -0,0 +1,27 @@
+package dabastock.om.dabactock.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RestController
+@RequestMapping("/supplier")
+public class SupplierController {
+    @GetMapping("/get_supplier")
+    public void getSupplier() {
+
+    }
+    @PostMapping("/post_supplier")
+    public void postSupplier() {
+
+    }
+    @PutMapping("/update_supplier")
+    public void updateSupplier() {
+
+    }
+    @DeleteMapping("/delete_supplier")
+    public void deleteSupplier() {
+
+    }
+
+}
