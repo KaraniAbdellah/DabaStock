811ec74b47a95efa13b0d4f41aba79af24b4296c
KaraniAbdellah <abdellahkarani@gmail.com>
2025-06-09 21:54:33 +0100

    add supplier service

--- frontend/src/app/services/suppliers/supplier.service.ts
+++ frontend/src/app/services/suppliers/supplier.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SupplierService {
+  private url = "http://127.0.0.1:8080/supplier";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getSupplier() {
+    return this.http.get(`${this.url}/get_supplier`);
+  }
+
+  postSupplier(data: any) {
+    return this.http.get(`${this.url}/post_supplier`, data);
+  }
+
+  updateSupplier(data: any,  id: any) {
+    return this.http.get(`${this.url}/update_supplier/${id}`, data);
+  }
+
+  deleteSupplier(id: any) {
+    return this.http.get(`${this.url}/delete_supplier/${id}`);
+  }
 }
+
