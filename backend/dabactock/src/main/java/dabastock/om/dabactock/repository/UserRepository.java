2b0c2ff4494b23e77fa81bf560cacea545ed9841
KaraniAbdellah <abdellahkarani@gmail.com>
2025-06-13 23:09:47 +0100

    add User Class, User Repository and User Controller

---
+++ backend/dabactock/src/main/java/dabastock/om/dabactock/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package dabastock.om.dabactock.repository;
+
+import dabastock.om.dabactock.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
